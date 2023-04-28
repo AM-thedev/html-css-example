fe9eddd8247487728d91385c4700908217b9cb9a
A.M <austincmckee@gmail.com>
2023-04-28 10:56:08 +0900

    unified sitemap

---
+++ js/accordion.js
@@ -0,0 +1,47 @@
+const acc = document.getElementsByClassName("js-accordion");
+const desktopMode = window.matchMedia("(min-width: 768px)");
+
+desktopMode.addEventListener('change', function(e) {
+  for (let i = 0; i < acc.length; i++) {
+    let accordion = acc[i];
+    if (accordion.classList.contains('js-accordion--desktopExpand')) {
+      let content = accordion.nextElementSibling;
+      if (e.matches) {
+        console.log('DESKTOP MODE');
+        accordion.classList.add("active");
+        content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+        accordion.classList.remove("active");
+        content.style.maxHeight = null;
+      }
+    }
+  }
+})
+
+for (let i = 0; i < acc.length; i++) {
+  let accordion = acc[i];
+  if (accordion.classList.contains('js-accordion--desktopExpand')) {
+    if (desktopMode.matches) {
+      accordion.classList.toggle("active");
+      let content = accordion.nextElementSibling;
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+
+  accordion.addEventListener("click", function() {
+    if (this.classList.contains('js-accordion--desktopExpand') && desktopMode.matches) {
+      console.log('CLICKED WHILE DESKTOP');
+      return;
+    }
+
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      var parent = this.parentElement;
+      parent.style.maxHeight = parseInt(parent.style.maxHeight) + content.scrollHeight + "px";
+    }
+  });
+}
